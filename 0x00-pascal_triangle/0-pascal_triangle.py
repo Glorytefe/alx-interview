9d2c61846ba593c836fbaf74b7eb249dabcb03f3
glowcoder1 <tefeglory.official@gmail.com>
2024-08-06 20:51:52 +0100

    optimize paschal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,21 +15,10 @@ def pascal_triangle(n):
     triangle = [[1]]
     if n == 1:
         return triangle
-    triangle.append([1, 1])
-    if n == 2:
-        return triangle
     while len(triangle) < n:
-        triangleLen = len(triangle)
-        expectedRowLen = triangleLen + 1
-        row = []
-        prevRow = triangle[triangleLen - 1]
-        for i in range(expectedRowLen):
-            if i == 0 or i == expectedRowLen - 1:
-                # means it's the edge of the row
-                row.append(1)
-            else:
-                # add the 2 col(i - 1 & n) in the prev row to get curr val
-                currVal = prevRow[i - 1] + prevRow[i]
-                row.append(currVal)
+        row = [1]
+        prevRow = triangle[-1]
+        row.extend(prevRow[i] + prevRow[i + 1] for i in range(len(prevRow) - 1))
+        row.append(1)
         triangle.append(row)
     return triangle
